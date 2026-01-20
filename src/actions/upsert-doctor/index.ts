06614addbae3a276f6a8eb112a189d55b18e41b6
Eduardo Leite Ranzzani <eduranzzani@gmail.com>
2026-01-20 10:12:26 -0400

    feat: add dayjs for time conversion and update upsertDoctor to handle UTC times

--- src/actions/upsert-doctor/index.ts
+++ src/actions/upsert-doctor/index.ts
@@ -3,12 +3,38 @@ import { db } from '@/db';
 import { doctorsTable } from '@/db/schema';
 import { auth } from '@/lib/auth';
 import { actionClient } from '@/lib/next-safe-action';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
 import { headers } from 'next/headers';
 import { upsertDoctorSchema } from './schema';
+
+dayjs.extend(utc);
 
 export const upsertDoctor = actionClient
 	.schema(upsertDoctorSchema)
 	.action(async ({ parsedInput }) => {
+		const availableFromTime = parsedInput.availableFromTime; // 08:00:00 -> 12:00:00
+		const availableToTime = parsedInput.availableToTime; // 18:00:00 -> 22:00:00
+
+		console.log('availableFromTime', availableFromTime);
+		console.log('availableToTime', availableToTime);
+
+		const availableFromTimeUTC = dayjs()
+			.set('hour', parseInt(availableFromTime.split(':')[0]))
+			.set('minute', parseInt(availableFromTime.split(':')[1]))
+			.set('second', parseInt(availableFromTime.split(':')[2] ?? '00'))
+			.utc()
+			.format('HH:mm:ss');
+		const availableToTimeUTC = dayjs()
+			.set('hour', parseInt(availableToTime.split(':')[0]))
+			.set('minute', parseInt(availableToTime.split(':')[1]))
+			.set('second', parseInt(availableToTime.split(':')[2] ?? '00'))
+			.utc()
+			.format('HH:mm:ss');
+
+		console.log('availableFromTimeUTC', availableFromTimeUTC);
+		console.log('availableToTimeUTC', availableToTimeUTC);
+
 		const session = await auth.api.getSession({
 			headers: await headers(),
 		});
@@ -19,14 +45,18 @@ export const upsertDoctor = actionClient
 		await db
 			.insert(doctorsTable)
 			.values({
+				...parsedInput,
 				id: parsedInput.id,
 				clinicId: session?.user?.clinic?.id,
-				...parsedInput,
+				availableFromTime: availableFromTimeUTC,
+				availableToTime: availableToTimeUTC,
 			})
 			.onConflictDoUpdate({
 				target: [doctorsTable.id],
 				set: {
 					...parsedInput,
+					availableFromTime: availableFromTimeUTC,
+					availableToTime: availableToTimeUTC,
 				},
 			});
 	});
